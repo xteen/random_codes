9abe25dcb73375244d54a7546c0634d5e25b291c
xteen <viveksaxenaofficial@gmail.com>
2016-04-21 00:27:33 +0530

    new

--- quick.c
+++ quick.c
@@ -1,57 +1,81 @@
 #include<stdio.h>
-int main()
+#include<math.h>
+#include<stdlib.h>
+#include<sys/time.h>
+#define max 10000
 
+void swap(int a[],int i,int j)
 {
-	int array[20],n,i;
-	printf("Enter the size of the array");
-	scanf("%d",&n);
-	
-	printf("Enter the elements of the array.\n");
-	for (i=0;i<=0;i++)
-	scanf("%d",&array[i]);
-	quicksort(array,0,n-1);
-	
-	printf("Sorted array is.\n");
-	for(i=0;i<=n;i++)
-	printf("|%d|",array[i]);
-	
-
-	return 0;
+   int temp;
+   temp=a[i];
+   a[i]=a[j];
+   a[j]=temp;
 }
 
-
-int quick_sort(int *array,int low,int high)
+int partition(int a[],int l,int r)
 {
-	int pivot,i,j,temp;
-
-
-	if(low<high){
-	pivot = low;
-	i=low;
-	j=high;
-	while(i<j){
-		while (array [i] < array[pivot] && i <= high)
-		i++;
-		while(array[j]>array[pivot] && j >= low)
-		j--;
-	
-
-	if (i < j) {
-		temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-		}
-		}
-	temp = array[j];
-	array[j] = array[pivot];
-	array[pivot] = temp;
-
-
-	quicksort(array, low,j-1);
-	quicksort(array, j+1,high);
-}
+  int p=a[l];
+  int i=l+1;
+  int j=r;
+  while(i<=j)
+  {
+   while(a[i]<=p && i<=r)
+   {
+    i=i+1;
+   }
+   while(a[j]>p && j>=0)
+   {
+    j=j-1;
+   }
+   if(i<=j)
+   {
+    swap(a,i,j);
+   }
+  }
+   swap(a,l,j);
+   return j;
 }
 
+void qs(int a[],int l,int r)
+{
+ int s;
+ if(l<r)
+ {
+   s=partition(a,l,r);
+   qs(a,l,s-1);
+   qs(a,s+1,r);
+ }
+}
 
 
+void main()
+{
+  	int a[max],n,i,r,l;
+  	struct timeval t;
+  	double start,end;
+	FILE *fp;
+	fp = fopen("plots.txt","w");
+	for(n=100;n<=1000;n=n+100)
+	{
+		for(i=0;i<n;i++)
+		{
+  			a[i]=rand()%1000;
+  			//printf("\n %d \n",a[i]);
+			}
+			l=0;
+			r=n-1;
+			gettimeofday(&t,NULL);
+			start=t.tv_sec+(t.tv_usec/1000000.00);
+			qs(a,0,n-1);
+			gettimeofday(&t,NULL);
+			end=t.tv_sec+(t.tv_usec/1000000.00);
+			printf("\nTime taken for %d ele %lf \n ",n,end-start);
+			printf("sorted elements are:");
+			fprintf(fp,"%d \t %lf \n ",i,end-start);
+			
 
+	}
+    
+  fclose(fp);
+  system("gnuplot");
+}	
